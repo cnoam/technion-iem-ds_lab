3c36150a5a537e6e29a0d099bc5166d442353412
Ubuntu <cnoam@homework-tester.modhioxug00ulmjzngugprapkg.ax.internal.cloudapp.net>
2019-06-17 09:58:53 +0000

    Add back using user "nobody" for docker
    NOTE: make sure this user can write/delete files in $HOME/data/logs in the host machine

--- run_docker.sh
+++ run_docker.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
  IMAGE=$1
  mkdir -p $HOME/data/logs
+# chmod -R 777 $HOME/data/logs
  docker run -d --mount type=bind,source=$HOME/data,target=/app/data,readonly \
  			  --mount type=bind,source=$HOME/data/logs,target=/logs\
              -p80:8000 \
+	     --user nobody \
 	     --restart unless-stopped \
              $IMAGE
 
