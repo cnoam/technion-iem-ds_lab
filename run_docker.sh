604b9f97caa29139c8c0d6dc0281e92d54aafaba
Ubuntu <cnoam@homework-tester.modhioxug00ulmjzngugprapkg.ax.internal.cloudapp.net>
2019-06-17 08:52:00 +0000

    minor improve to run_docker

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
  IMAGE=$1
+ mkdir -p $HOME/data/logs
  docker run -d --mount type=bind,source=$HOME/data,target=/app/data,readonly \
  			  --mount type=bind,source=$HOME/data/logs,target=/logs\
              -p80:8000 \
