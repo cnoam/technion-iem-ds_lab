74a1001e0f57e75120a16f7c2ff628c2a1e54b9a
Noam Cohen <cnoam@technion.ac.il>
2020-08-10 09:20:20 +0300

    add check hw3 in 94219

---
+++ serverpkg/runners/check_java_file_ex3.sh
@@ -0,0 +1,49 @@
+#!/bin/bash -ex
+# custom made for semester: 2020Spring, course:094219, ex:3
+# $2 contains the full path to the data dir
+echo Running "$1" "$2" "$3" "$4"
+echo Timeout set to "$UUT_TIMEOUT"
+
+INPUT_SRC=`realpath $1`
+INPUT_DATA=`realpath $2`
+GOLDEN=`realpath $3`
+COMPARATOR=`realpath $4`
+DATA_DIR=/hw3data/
+
+TESTDIR=`mktemp -d`
+pushd /tmp
+rm -rf $TESTDIR
+mkdir $TESTDIR
+cd $TESTDIR
+echo PWD="`pwd`"
+
+#unshare -rn /usr/bin/time  -f "run time: %U user %S system" timeout $UUT_TIMEOUT java $1 $2 $3 $4
+# if there is an error, this line is NOT executed ( "-e" )
+# ...
+
+# extract the zip, cd to src, compile and run
+unzip $INPUT_SRC
+cd src
+
+fname=DocumentRetrieval
+javac $fname.java
+
+echo ==== compilation OK ====
+/usr/bin/time  -f "run time: %U user %S system"  timeout $UUT_TIMEOUT java $fname  $DATA_DIR $INPUT_DATA > output
+
+echo ==== finished the tested run ====
+set +e
+
+echo Comparing output , $GOLDEN
+python3 $COMPARATOR output $GOLDEN
+retVal=$?
+if [ $retVal -eq 42 ]; then
+    echo "Sorry: actual output is different from the required output"
+    exit 42
+fi
+if [ $retVal -ne 0 ]; then
+    echo "Sorry: some error occured. Please examine the STDERR"
+    exit 43
+fi
+popd
+echo ---------- run OK
