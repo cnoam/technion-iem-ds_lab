c5eed0256e57a0a7e70fde1bbadb0a9d8a883ed8
Noam Cohen <cnoam@technion.ac.il>
2021-01-17 17:04:16 +0000

    ignore permission error chmod of mounted files
    user nobody creates files with 0644 so user 'cnoam'
    cannot chmod it. Ignore this since it should be benign

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
  IMAGE=$1
+
+set +e
  mkdir -p $HOME/checker/data/{db,logs}
  chmod -R 777 $HOME/checker/data/{db,logs}
-
+set -e
  docker run -d --mount type=bind,source=$HOME/checker/data,target=/data,readonly \
  	       --mount type=bind,source=$HOME/checker/data/logs,target=/logs\
 	       --mount type=bind,source=$HOME/checker/data/db,target=/db\
