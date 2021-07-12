ab99efb1cc236623203f2006956eb240405dca6f
Noam Cohen <cnoam@technion.ac.il>
2021-07-12 15:42:31 +0300

    refactor the Docker files
    Each course (actually a programming environment) now has its own install variant.

--- install.sh
+++ install.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -ex
+#!/bin/bash -eu
+# Install the needed packages to run the homework checker
+# The argument USE_LANG can be }
 
 # this script has to run in the git repo root directory.
+USE_LANG=$1
+
+
+echo Cloning repo of per-course code and data
+git clone https://github.com/noam1023/technion_checker_data.git checker_data
+
 echo Installing Checker on ubuntu flavoured machine
 sudo apt-get update -y && sudo apt-get upgrade -y
 sudo apt-get install -y docker.io
@@ -17,11 +25,36 @@ sudo systemctl enable docker
 set -e
 
 # Currently need to manually build the dependency images
-docker build -t python_cmake_base -f Dockerfile_base .
-docker build -t py_java_cpp_base -f Dockerfile_py_java_cpp_base .
-docker build -t server .
+case $USE_LANG in
+   python|spark)
+   docker build -t python_base -f Dockerfile_py_base .
+   ;;
+   
+   cpp)
+   docker build -t server_cpp -f Dockerfile_cpp .
+   ;;
+   
+   java)
+   docker build -t py_java -f Dockerfile_py_java .
+   ;;
+   
+   xv6)
+   docker build -t server_cpp -f Dockerfile_cpp .
+   docker build -t server_xv6 -f Dockerfile_xv6 .
+   ;;
+   
+   
+   *)
+   echo bad argument value.
+   exit 1
+esac
+   
+
 
 # TODO: add log rotation (to the disk will not be filled)
 # https://docs.docker.com/config/containers/logging/configure/#configure-the-default-logging-driver
+
+echo Adding path to checker_data to .bashrc
+echo export CHECKER_DATA_DIR=`pwd`/checker_data >> ~/.bashrc
 
 echo "DONE."
